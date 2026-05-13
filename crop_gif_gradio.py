e1956feeb1dc4987043d3a996e77243281fffbb4
Anirudh <asundararaj2@wisc.edu>
2026-05-13 00:55:17 -0500

    cropped gifs

--- crop_gif_gradio.py
+++ crop_gif_gradio.py
@@ -183,4 +183,4 @@ with gr.Blocks(title="GIF Cropper") as demo:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "7860"))
-    demo.launch(server_name="0.0.0.0", server_port=port)
+    demo.launch(server_name="0.0.0.0", server_port=port, share=True)
